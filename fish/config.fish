b2455d91152473c5fdc140f92f71cbbc86ebceb9
Karan Singh <i-am-karan-singh@users.noreply.github.com>
2026-04-11 03:11:34 -0400

    merging with arch

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,21 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+switch (uname)
+    case Darwin
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+        export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+        export HOMEBREW_NO_ENV_HINTS=TRUE
+
+        alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+
+        set -gx PATH $PATH /Users/karan/.lmstudio/bin
+    case Linux
+        set -gx PATH $PATH /home/karan/.lmstudio/bin
+
+        set -gx DOCKER_HOST unix:///run/user/1000/podman/podman.sock
+    case '*'
+        # do things for other OSs
+end
 
 set -U fish_greeting
 
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-export HOMEBREW_NO_ENV_HINTS=TRUE
-
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
-
 fzf --fish | source
 bind -M insert "ç" fzf-cd-widget
-
-# Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/karan/.lmstudio/bin
-# End of LM Studio CLI section
-
